b9e3a87ae52fca5edbea91f231218783b68f65c7
EgorZapatylak <zapatylak96a@gmail.com>
2022-11-08 01:02:11 +0300

    Updates

---
+++ precache-manifest.8b4dad48e65a7dd06e0993239e8dc47c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f4fe49ac479b496052c",
+    "url": "/parallax_web/static/css/main.3409af53.chunk.css"
+  },
+  {
+    "revision": "4f4fe49ac479b496052c",
+    "url": "/parallax_web/static/js/main.4f4fe49a.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/parallax_web/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/parallax_web/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "3cb78dfcf484d170df44",
+    "url": "/parallax_web/static/js/runtime~main.3cb78dfc.js"
+  },
+  {
+    "revision": "957a49ea7b14d2e2181a33447e75821a",
+    "url": "/parallax_web/static/media/raleway-regular.957a49ea.woff2"
+  },
+  {
+    "revision": "294f721cf4243daa11adfee5139c472c",
+    "url": "/parallax_web/static/media/raleway-black.294f721c.woff2"
+  },
+  {
+    "revision": "00fdadb24ec4d915fa76ee8accede35d",
+    "url": "/parallax_web/static/media/dungeon.00fdadb2.jpg"
+  },
+  {
+    "revision": "859e14f4a15cd98ad7bcc32a30e9c073",
+    "url": "/parallax_web/static/media/layer-middle.859e14f4.png"
+  },
+  {
+    "revision": "76998f8662876ebbe129fdbbc7e7e467",
+    "url": "/parallax_web/static/media/layer-front.76998f86.png"
+  },
+  {
+    "revision": "e17d3f76756105d715abc8e20d7e5215",
+    "url": "/parallax_web/static/media/layer-base.e17d3f76.png"
+  },
+  {
+    "revision": "4bdbbd17202907ce7a5ff8797d58f6f3",
+    "url": "/parallax_web/index.html"
+  }
+];
