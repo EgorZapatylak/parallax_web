4999a32ba5b53c5f7843996b590483a2fb5b5461
Egor Zapatylak <zapatylak96a@gmail.com>
2022-11-09 14:43:58 +0300

    Added some future upgrade in app.js

--- src/components/js/app.js
+++ src/components/js/app.js
@@ -1,3 +1,9 @@
 window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${window.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
 })
+
+// gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+// ScrollSmoother.create({
+//     wrapper: '.wrapper',
+//     content: '.content'
+// })
